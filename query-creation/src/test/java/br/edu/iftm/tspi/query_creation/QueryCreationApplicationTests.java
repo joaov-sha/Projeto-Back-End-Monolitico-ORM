4a059dda4e6bd5b2e766272b9f7e27624c92344f
joaov-sha <joaovitor8832@gmail.com>
2024-09-12 18:50:14 -0300

    Code cleanup - S1186 - Adição de comentário explicando porquê o a classe está vazia

---
+++ query-creation/src/test/java/br/edu/iftm/tspi/query_creation/QueryCreationApplicationTests.java
@@ -0,0 +1,14 @@
+package br.edu.iftm.tspi.query_creation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueryCreationApplicationTests {
+
+	@Test
+	void contextLoads() {
+		// Método gerado automaticamente na criação do arquivo
+	}
+
+}
